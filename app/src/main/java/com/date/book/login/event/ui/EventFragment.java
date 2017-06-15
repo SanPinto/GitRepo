4785735d9cd891cbbbc858a2cbdfddc7de8e22f0
sangeetha.pinto <sangeetha.pinto@robosoftin.com>
2017-06-15 18:48:29 +0530

    Created Event Screen initial commit

--- app/src/main/java/com/date/book/login/event/ui/EventFragment.java
+++ app/src/main/java/com/date/book/login/event/ui/EventFragment.java
@@ -1,10 +1,24 @@
 package com.date.book.login.event.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.date.book.R;
 
 /**
  * Created by sangeetha on 14/6/17.
  */
 
 public class EventFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.event_fragment, container, false);
+        return view;
+    }
 }
